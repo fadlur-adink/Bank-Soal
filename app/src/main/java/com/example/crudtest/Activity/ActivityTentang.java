f3b1d713230296c6c9b12e0b5c94661aae1f095f
fadlur-adink <fadlurrahman1@outlook.com>
2020-07-11 19:11:45 +0700

    Penambahan Halaman Tentang

--- app/src/main/java/com/example/crudtest/Activity/ActivityTentang.java
+++ app/src/main/java/com/example/crudtest/Activity/ActivityTentang.java
@@ -1,8 +1,10 @@
-package com.example.crudtest;
+package com.example.crudtest.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.crudtest.R;
 
 public class ActivityTentang extends AppCompatActivity {
 
--- app/src/main/java/com/example/crudtest/Activity/HomeActivity.java
+++ app/src/main/java/com/example/crudtest/Activity/HomeActivity.java
@@ -8,7 +8,6 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.example.crudtest.ActivityTentang;
 import com.example.crudtest.Adapter.SharedPrefManager;
 import com.example.crudtest.R;
 
